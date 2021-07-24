1c9260657fe05dd353afb4e23ce25fbdf5395019
Kurnakova <kurnakovaanastasia49@gmail.com>
2021-07-24 09:05:12 +0300

    Add cupboard model with CRUD methods

---
+++ PotionStoreConsole/Models/Cupboard.cs
@@ -0,0 +1,129 @@
+using System.Collections.Generic;
+using System.Linq;
+using Enums;
+
+namespace Models
+{
+    public class Cupboard
+    {
+        public int Id { get; set; }
+        public List<PotionsInformationClass> Potions { get; set; }
+
+        public Cupboard()
+        {
+            Potions = SetPotions();
+        }
+
+        public void AddNewPotion(PotionsInformationClass potion)
+        {
+            // TODO: Добавить обработчиков для валидации.
+            Potions.Add(potion);
+        }
+
+        public PotionsInformationClass GetPotionById(int potionId)
+        {
+            var potion = Potions.FirstOrDefault(p => p.PotionID == potionId);
+
+            // TODO: Ввести проверку на null
+
+            return potion;
+        }
+
+        public void DeletePotionById(int potionId)
+        {
+            var potion = GetPotionById(potionId);
+            Potions.Remove(potion);
+        }
+
+        public void EditPotion(int potionId, string newTitle, Effect newEffect, string newDescription)
+        {
+            var potion = GetPotionById(potionId);
+
+            // TODO: Провалидировать входные данные.
+
+            potion.Title = newTitle;
+            potion.Effect = newEffect;
+            potion.Description = newDescription;
+        }
+
+
+        private List<PotionsInformationClass> SetPotions()
+        {
+            List<PotionsInformationClass> potions = new List<PotionsInformationClass>()
+            {
+                new PotionsInformationClass()
+                {
+                    PotionID = 0,
+                    Title = "Целебное Зелье",
+                    Effect = Effect.Positive,
+                    Description = "Это зелье излечивает любые раны.",
+                },
+                new PotionsInformationClass()
+                {
+                    PotionID = 1,
+                    Title = "Гадкое Зелье",
+                    Effect = Effect.Negative,
+                    Description = "Вызывает несварение желудка. Доза больше 200 мл может привести к смерти."
+                },
+                new PotionsInformationClass()
+                {
+                    PotionID = 2,
+                    Title = "Приворотное Зелье",
+                    Effect = Effect.Neutral,
+                    Description = "Добавьте в зелье человеческий волос и человек, выпивший получившийся напиток, влюбится в обладателя волоса. Желательно класть волосы ОДНОГО человека и не того же, кто пьет зелье. Несоблюдение инструкции может привести к непредсказуемым результатам, но если вас это не пугает - веселитесь ;p.",
+                },
+                new PotionsInformationClass()
+                {
+                    PotionID = 3,
+                    Title = "Зелье Молодости",
+                    Effect = Effect.Positive,
+                    Description = "100 грамм зелья омоложает выпившего на 20 лет. Если ему меньше лет, он превращается в новорожденного.",
+                },
+                new PotionsInformationClass()
+                {
+                    PotionID = 4,
+                    Title = "Зелье Вампиризма",
+                    Effect = Effect.Negative,
+                    Description = "Превращает выпившего в вампира."
+                },
+                new PotionsInformationClass()
+                {
+                    PotionID = 5,
+                    Title = "Зелье Удачи",
+                    Effect = Effect.Positive,
+                    Description = "Выпивший становится удачливым на 24 часа."
+                },
+                new PotionsInformationClass()
+                {
+                    PotionID = 6,
+                    Title = "Зелье Ясновидения",
+                    Effect = Effect.Neutral,
+                    Description = "Выпивший повышает свои способности к ясновидению. Если он не проведет обряд гадания, ему приснится сон, который поведает ему о будущем."
+                },
+                new PotionsInformationClass()
+                {
+                    PotionID = 7,
+                    Title = "Вредное Зелье",
+                    Effect = Effect.Negative,
+                    Description = "Выпивший покрывается бородавками. Если не пытаться свести их никакими средствами, они проходят через 29 дней."
+                },
+                new PotionsInformationClass()
+                {
+                    PotionID = 8,
+                    Title = "Противозельевое Зелье",
+                    Effect = Effect.Positive,
+                    Description = "Устраняет эффект любого зелья (кроме тех, которые по природе своей не могут быть устранены таким образом)."
+                },
+                new PotionsInformationClass()
+                {
+                    PotionID = 9,
+                    Title = "Сырное зелье",
+                    Effect = Effect.Neutral,
+                    Description = "Выпивший это зелье превращается в сыр."
+                }
+            };
+
+            return potions;
+        }
+    }
+}
